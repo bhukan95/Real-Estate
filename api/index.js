ae9ee31b4aa0aaf2f8fb038ab5d3a7bb38ff2db9
bhukan95 <prakashbhukan95@gmail.com>
2024-06-03 13:41:01 +0530

    create a test api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
 import userRouter from './routes/user.route.js'
+import authRouter from './routes/auth.route.js'
 
 mongoose.connect(process.env.MONGO).then(()=>{
  console.log('Connected to mongoDB!');
@@ -11,9 +12,11 @@ mongoose.connect(process.env.MONGO).then(()=>{
 });
 
 const app = express();
+app.use(express.json());
 
 app.listen(3000,()=>{
     console.log('Server is running on port 3000 !!');
 });
 
-app.use('/api/user',userRouter)
+app.use('/api/user',userRouter);
+app.use('/api/auth',authRouter);
