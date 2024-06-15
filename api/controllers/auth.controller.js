cd437a6d9a825de32f89e52e183909c3d072c64b
bhukan95 <prakashbhukan95@gmail.com>
2024-06-15 16:15:21 +0530

    Create a sign in api

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -2,6 +2,7 @@ import { FcNext } from "react-icons/fc";
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
 import { errorHandler } from "../utils/error.js";
+import jwt from 'jsonwebtoken';
                                                           
 export const signup = async (req,res,next)=>{
 
@@ -15,3 +16,26 @@ export const signup = async (req,res,next)=>{
         next(error);                                                             
     } 
 }; 
+export const signin = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Check if email and password are present
+    if (!email || !password) {
+        return next(errorHandler(400, 'Email and Password are required'));
+    }
+
+    try {
+        const validUser = await User.findOne({ email });
+        if (!validUser) return next(errorHandler(404, 'User Not Found!'));
+
+        const validPassword = bcryptjs.compareSync(password, validUser.password);
+        if (!validPassword) return next(errorHandler(401, 'Wrong Credentials!'));
+
+
+        const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+        const {password:pass,...rest} = validUser._doc;
+        res.cookie('access_token', token, { httpOnly: true }).status(200).json(rest);
+    } catch (error) {
+        next(error);
+    }
+};
--- api/index.js
+++ api/index.js
@@ -2,32 +2,37 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
-import userRouter from './routes/user.route.js'
-import authRouter from './routes/auth.route.js'
-
-mongoose.connect(process.env.MONGO).then(()=>{
- console.log('Connected to mongoDB!');
-}).catch((err)=>{
-    console.log(err);
-});
+import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 const app = express();
+
+// Middleware to parse JSON bodies
 app.use(express.json());
-app.listen(3000,()=>{
-    console.log('Server is running on port 3000 !!');
-});
 
-app.use('/api/user',userRouter);
-app.use('/api/auth',authRouter);
+// Routes
+app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
 
-app.use((err,req,res,next)=>{
+// Error handling middleware
+app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
     return res.status(statusCode).json({
-         success: false,
-         statusCode,
-         message,
+        success: false,
+        statusCode,
+        message,
     });
-}); 
+});
 
+// Connect to MongoDB
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('Connected to mongoDB!');
+}).catch((err) => {
+    console.log(err);
+});
 
+// Start the server
+app.listen(3000, () => {
+    console.log('Server is running on port 3000 !!');
+});
